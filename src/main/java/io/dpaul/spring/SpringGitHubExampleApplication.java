135d163f40717323d2fbdcc961e034df9edd2c32
dpaul91 <debangshu.alt@gmail.com>
2022-05-07 11:39:07 +0530

    Spring-demo of oauth using Github

---
+++ src/main/java/io/dpaul/spring/SpringGitHubExampleApplication.java
@@ -0,0 +1,25 @@
+package io.dpaul.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringGitHubExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGitHubExampleApplication.class, args);
+	}
+
+	@GetMapping("/user")
+	public String user(@AuthenticationPrincipal OAuth2User principal) {
+		System.out.println(principal);
+		return principal.getName();
+	}
+
+}
---
+++ src/test/java/io/dpaul/spring/SpringGitHubExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.dpaul.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGitHubExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
